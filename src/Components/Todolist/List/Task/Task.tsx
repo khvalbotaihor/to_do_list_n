f6d794fa844720c2373b6eb56a67a04b78494b87
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-16 13:49:07 +0200

    2 - added classname instead traditional if else conditions to task component

--- src/Components/Todolist/List/Task/Task.tsx
+++ src/Components/Todolist/List/Task/Task.tsx
@@ -1,14 +1,24 @@
 import styles from "./Task.module.css";
 import React from "react";
+import cn from "classnames"
+
 
 type TaskType = {
     title: string
     isDone: boolean
 }
 export const Task = (props: TaskType) => {
-    const css = props.isDone ? styles.done : ""
+    const css1 = cn({
+        [styles.task]: true,
+        [styles.done]: props.isDone
+    })
+
+
+    //const css = props.isDone ? styles.done : ""
+
+
     return (
-        <div className={`${css} ${styles.task}`}>
+        <div className={css1}>
             <input type="checkbox" checked={props.isDone}/>
             <span>{props.title}</span>
         </div>
