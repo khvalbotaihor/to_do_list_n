dd95664edb677d6399b7da9aac373ad33354579e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-16 12:39:18 +0200

    2 - created new object and created a type for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './App.module.css';
 import {ToDoList} from "../src/Components/Todolist/ToDoList";
+import {v1} from "uuid";
+
+type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
 
 function App() {
+
+    const tasks : TaskType[]= [
+        {id: v1(), title:"CSS", isDone:true},
+        {id: v1(), title:"JS", isDone:true},
+        {id: v1(), title:"React", isDone:false},
+    ]
+
     return (
         <div className={styles.App}>
             <ToDoList />
